45267f6b5530ff29e29b9936973d215048e57ef8
kolyb <kolybenko@mail.ru>
2023-09-24 13:02:30 +0300

    Create Configuration

---
+++ source/DailyReport.BusinessLogic/Configuration/DailyReportBusinessLogicConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using DailyReport.BusinessLogic.Interfaces;
+using DailyReport.BusinessLogic.Interfaces.IdentityInterface;
+using DailyReport.BusinessLogic.ModelsDTO;
+using DailyReport.BusinessLogic.Servicies;
+using DailyReport.BusinessLogic.Servicies.IdentityService;
+using DailyReport.DataAccess.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyReport.BusinessLogic.Configuration
+{
+    public static class DailyReportBusinessLogicConfiguration
+    {
+        public static void ConfigureDailyReportBusinessLogic(this IServiceCollection services)
+        {
+            services.ConfigureDailyReportDataAccess();
+            services.AddScoped<IUserIdentity, UserIdentityService>();
+            services.AddScoped<IService<PersonDTO>, PersonService>();
+            services.AddScoped<IService<WorkLocationDTO>, WorkLocationService>();
+            services.AddScoped<IService<EventDTO>, EventService>();
+
+        }
+    }
+}
