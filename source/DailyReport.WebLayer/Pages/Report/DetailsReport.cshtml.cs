5e6308d1cafa8b99ff945e5e5984655bcb89efc8
kolyb <kolybenko@mail.ru>
2023-10-14 20:07:02 +0300

    Create DetailsReport

---
+++ source/DailyReport.WebLayer/Pages/Report/DetailsReport.cshtml.cs
@@ -0,0 +1,49 @@
+using DailyReport.BusinessLogic.Interfaces;
+using DailyReport.BusinessLogic.ModelsDTO;
+using DailyReport.WebLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DailyReport.WebLayer.Pages.Report
+{
+    public class DetailsReportModel : PageModel
+    {
+        private readonly IService<PersonDTO> _servicePersonDTO;
+        private readonly IService<ReportDTO> _serviceReportDTO;
+        private readonly IService<PlanDateDTO> _servicePlanDateDTO;
+
+        public DetailsReportModel(IService<PersonDTO> servicePersonDTO,
+            IService<ReportDTO> serviceReportDTO,
+            IService<PlanDateDTO> servicePlanDateDTO)
+        {
+            _servicePersonDTO = servicePersonDTO;
+            _serviceReportDTO = serviceReportDTO;
+            _servicePlanDateDTO = servicePlanDateDTO;
+        }
+
+        public List<PersonDTO>? PersonDTOs { get; set; }
+
+        public List<ReportDTO>? ReportDTOs { get; set; }
+
+        public List<PlanDateDTO>? PlanDateDTOs { get; set; }
+
+        public List<PlanLastnameViewModel>? ReportLastnames { get; set; }
+
+        public void OnGetAsync(int id)
+        {
+            ReportDTOs = _serviceReportDTO.GetAll().ToList().Where(i => i.PlanDateId == id).ToList();
+            PersonDTOs = _servicePersonDTO.GetAll().ToList();
+            ReportLastnames = (from p in PersonDTOs
+                             join rp in ReportDTOs
+                             on p.Id equals rp.PersonId
+                             orderby rp.Time
+                             where rp.PlanDateId == id
+                             select new PlanLastnameViewModel { DateTime = rp.Time, Lastname = p.LastName }).ToList();
+        }
+
+        public ActionResult OnPostCancel()
+        {
+            return RedirectToPage("Index");
+        }
+    }
+}
